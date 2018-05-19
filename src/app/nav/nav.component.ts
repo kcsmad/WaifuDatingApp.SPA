9473479ccb8c2ab04077d16e7ac996c0cf26ecfd
Kevin Maduro <nivekzin@gmail.com>
2018-05-19 18:19:13 -0300

    Section 7 - Lecture 63

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import {AuthService} from '../_services/auth.service';
 import {AlertifyService} from '../_services/alertify.service';
 
@@ -12,7 +13,8 @@ export class NavComponent implements OnInit {
 
 
   constructor(private authService: AuthService,
-              private alertify: AlertifyService) { }
+              private alertify: AlertifyService,
+              private router: Router) { }
 
   ngOnInit() {
 
@@ -22,7 +24,9 @@ export class NavComponent implements OnInit {
     this.authService.login(this.model).subscribe(data => {
       this.alertify.success('Logged in successfully!');
     }, error => {
-      this.alertify.error(error);
+      this.alertify.error('Failed to Login');
+    }, () => {
+      this.router.navigate(['/members']);
     });
   }
 
