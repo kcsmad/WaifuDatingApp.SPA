a73cb7692059a13ab7a3fa6cd5457209bc22da27
Kevin Maduro <nivekzin@gmail.com>
2018-05-19 18:28:49 -0300

    Section 7 - Lecture 64

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -7,8 +7,15 @@ import {AuthGuard} from './_guards/auth.guard';
 
 export const appRoutes: Routes = [
   {path: 'home', component: HomeComponent},
-  {path: 'members', component: MemberListComponent, canActivate: [AuthGuard]},
-  {path: 'messages', component: MessagesComponent, canActivate: [AuthGuard]},
-  {path: 'lists', component: ListsComponent, canActivate: [AuthGuard]},
+  {
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [AuthGuard],
+    children: [
+      { path: 'members', component: MemberListComponent },
+      { path: 'messages', component: MessagesComponent },
+      { path: 'lists', component: ListsComponent },
+    ]
+  },
   {path: '**', redirectTo: 'home', pathMatch: 'full'},
 ];
