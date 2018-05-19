6254ba64154a4608563b40d864da66b174d79063
Kevin Maduro <nivekzin@gmail.com>
2018-05-19 16:09:52 -0300

    Section 7 - Lecture 61

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,25 +6,34 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 
-import { AuthService } from './services/auth.service';
+import { AuthService } from './_services/auth.service';
 import { HttpClientModule } from '@angular/common/http';
 import { JwtModule } from '@auth0/angular-jwt';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
-import { AlertifyService } from './services/alertify.service';
+import { AlertifyService } from './_services/alertify.service';
 import { BsDropdownModule } from 'ngx-bootstrap';
+import { MemberListComponent } from './member-list/member-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './routes';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    MemberListComponent,
+    ListsComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
     BsDropdownModule.forRoot(),
     HttpClientModule,
+    RouterModule.forRoot(appRoutes),
     JwtModule.forRoot({
       config: {
         tokenGetter: () => {
